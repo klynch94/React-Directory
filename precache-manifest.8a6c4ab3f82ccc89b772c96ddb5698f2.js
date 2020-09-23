ad246b59a060f530eb89d75dd122f86150f9e62f
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-22 21:41:23 -0500

    Updates

---
+++ precache-manifest.8a6c4ab3f82ccc89b772c96ddb5698f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d8c92e46084001f99c5edcc410dc31",
+    "url": "/React-Directory/index.html"
+  },
+  {
+    "revision": "661e9962bb7af884aedf",
+    "url": "/React-Directory/static/css/main.47b2d689.chunk.css"
+  },
+  {
+    "revision": "a374654d78c01ed909a4",
+    "url": "/React-Directory/static/js/2.11526a5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Directory/static/js/2.11526a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661e9962bb7af884aedf",
+    "url": "/React-Directory/static/js/main.26f32739.chunk.js"
+  },
+  {
+    "revision": "e46c8046b5d72e1ef96c",
+    "url": "/React-Directory/static/js/runtime-main.ae086268.js"
+  }
+]);
